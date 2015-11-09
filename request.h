f808f987c7aa2ea51df34af48c09356c39acb465
Zhenyuan Shen <szywind@163.com>
2015-11-08 19:28:33 -0600

    modified server.c and request.h

--- request.h
+++ request.h
@@ -2,4 +2,75 @@
 
 void requestHandle(int fd);
 
+enum ERROR_CODE{SUCCESS, ERROR};
+typedef struct _request_info_
+{
+  int is_static;               // denote whether the request is static or dynamic
+  char filename[MAXLINE];      // name of the requested file
+  int connfd;                  // connect file descriptor
+  enum ERROR_CODE error_code;  // error code: 0-SUCCESS, 1-ERROR
+} __request_info_;
+
+typedef struct _buffer_
+{
+	int buffer_size;           // size of the buffer
+	int head;                  // head index 
+	int length;                // length of the queue
+	__request_info_ * request;
+} __buffer_;
+
+
+int isBufEmpty(volatile __buffer_ * buf){
+	//return (buf->tail == buf->head)?1:0;
+	return buf->length == 0;
+}
+
+int isBufFull(volatile __buffer_ * buf){
+	//return ((buf->tail+1)%buf->buffer_size == buf->head)?1:0;
+	return buf->length == buf->buffer_size;
+}
+
+__request_info_ deBuf(volatile __buffer_ * buf){
+	__request_info_ temp = buf->request[buf->head];
+	buf->head = (buf->head+1)%buf->buffer_size;
+	buf->length --;
+	return temp;
+}
+
+void enBuf(volatile __buffer_ * buf, __request_info_ req){
+	//buf->tail = (buf->tail+1)%buf->buffer_size;
+	buf->request[buf->head+buf->length] = req;
+	buf->length ++;
+}
+
+
+volatile __buffer_ myBuffer;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+
+/* consumers */
+void * worker(void *arg) {
+	while(1){
+		printf("worker\n");
+		pthread_mutex_lock(&mutex);
+		while(isBufEmpty(&myBuffer)){
+			pthread_cond_wait(&empty, &mutex);
+		}
+		
+		__request_info_ req = deBuf(&myBuffer);
+		printf("connfd = %d\n", req.connfd);
+		requestHandle(req.connfd);
+	
+		Close(req.connfd);
+
+		pthread_cond_signal(&full);
+		pthread_mutex_unlock(&mutex);
+
+	}
+	return NULL;
+}
+
+
+
 #endif
--- server.c
+++ server.c
@@ -12,43 +12,90 @@
 //
 
 // CS537: Parse the new arguments too
-void getargs(int *port, int argc, char *argv[])
+void getargs(int *args, int argc, char *argv[])
 {
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+    if (argc != 4) {
+	fprintf(stderr, "Usage: %s <port> <threads> <buffers>\n", argv[0]);
 	exit(1);
     }
-    *port = atoi(argv[1]);
+    args[0] = atoi(argv[1]);
+    args[1] = atoi(argv[2]);
+    args[2] = atoi(argv[3]);	
 }
 
 
 int main(int argc, char *argv[])
 {
-    int listenfd, connfd, port, clientlen;
+    int listenfd, connfd, args[3], clientlen;
     struct sockaddr_in clientaddr;
 
-    getargs(&port, argc, argv);
-
+    getargs(args, argc, argv); // get the port number specified by users
+    int port = args[0];
+    int nThreads = args[1];
+    int szBuffer = args[2];
     // 
     // CS537: Create some threads...
-    //
-
-    listenfd = Open_listenfd(port);
-    while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
-
-	// 
-	// CS537: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work. However, for SFF, you may have to do a little work
-	// here (e.g., a stat() on the filename) ...
-	// 
-	requestHandle(connfd);
-
-	Close(connfd);
+    
+    // initialize the buffer
+    myBuffer.buffer_size = szBuffer;
+    myBuffer.head = 0;
+    myBuffer.length = 0;
+    myBuffer.request = NULL;
+    if(NULL == (myBuffer.request = (__request_info_ *)malloc(szBuffer*sizeof(__request_info_)))){
+  		perror("create buffer error!\n");
+		exit(1);  	
     }
 
+	// create thread pool
+	pthread_t * pool = NULL;
+	if(NULL == (pool = (pthread_t *) malloc(nThreads * sizeof(pthread_t)))){
+		perror("create pool error!\n");
+		exit(1);
+	}
+
+    // create workers
+    unsigned int i = 0;
+    for(; i<nThreads; i++){
+    	pthread_create(&pool[i], NULL, worker, NULL);
+    }
+
+    listenfd = Open_listenfd(port); // listening socket file descriptor
+    while (1) {
+		clientlen = sizeof(clientaddr);
+		connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+
+		// 
+		// CS537: In general, don't handle the request in the main thread.
+		// Save the relevant info in a buffer and have one of the worker threads 
+		// do the work. However, for SFF, you may have to do a little work
+		// here (e.g., a stat() on the filename) ...
+		// 
+
+	    /* producer */
+	    pthread_mutex_lock(&mutex);
+		while(isBufFull(&myBuffer)){
+			pthread_cond_wait(&full, &mutex);
+		}
+		
+		__request_info_ req;
+		req.connfd = connfd;
+
+		printf("connfd = %d\n", connfd);
+
+		enBuf(&myBuffer, req);
+
+		printf("sz = %d, head = %d, length = %d, request->connfd = %d\n",
+			myBuffer.buffer_size, myBuffer.head, myBuffer.length, myBuffer.request[0].connfd);
+		pthread_cond_signal(&empty);
+		pthread_mutex_unlock(&mutex);		
+
+		//requestHandle(connfd);
+
+		//Close(connfd);
+    }
+    // free the pool and buffer
+    free(pool);
+    free(myBuffer.request);
 }
 
 
